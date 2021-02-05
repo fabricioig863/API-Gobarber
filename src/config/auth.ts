fe8664c4dcd24aecb5008854a07592058b9aa0b3
Fabricio <fabricioig863@gmail.com>
2021-02-05 14:41:14 -0300

    modulo iniciando-back-end completo

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'de6b37a9aa80f48b997adc163129539b',
+    expiresIn: '1d',
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,8 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
 
 import User from '../models/User';
 
@@ -21,18 +23,20 @@ class AuthenticateUserService {
     const user = await usersRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw new Error('Incorret email/passaword combination.');
+      throw new AppError('Incorret email/passaword combination.', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error('Incorret email/passaword combination.');
+      throw new AppError('Incorret email/passaword combination.', 401);
     }
 
-    const token = sign({}, 'de6b37a9aa80f48b997adc163129539b', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
