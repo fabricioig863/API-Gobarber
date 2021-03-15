c43ad753ad23beb28336faf0e57d1cd9e1477e59
Fabricio <fabricioig863@gmail.com>
2021-03-15 17:26:05 -0300

    Refatoração e ddd

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,11 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
-import routes from './routes';
+import routes from './shared/routes';
 import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
+import AppError from './shared/errors/AppError';
 
-import './database';
+import './shared/database';
 
 const app = express();
 
@@ -17,18 +17,18 @@ app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-    if (err instanceof AppError){
-      return response
-        .status(err.statusCode)
-        .json({ status: 'error', message: err.message });
-    }
-
-    console.error(err);
-
+  if (err instanceof AppError) {
     return response
-      .status(500)
-      .json({ status: 'error', message: 'Internal server error' });
-  });
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
+  }
+
+  console.error(err);
+
+  return response
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error' });
+});
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
